eef9b19f5df9516bdec968a343fad72d8a9bc860
L0gan012 <logan.sauer.012@gmail.com>
2019-11-02 15:16:03 -0400

    added google services

--- caos/lib/login.dart
+++ caos/lib/login.dart
@@ -26,59 +26,76 @@ class LoginBodyFormState extends State<LoginBody> {
   @override
   Widget build(BuildContext buildContext) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
-        Text('Caos', ),
-        Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Text('Username', style: TextStyle(fontSize: 24)),
-                ],
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a username';
-                  }
-                  return null;
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Text('Password', style: TextStyle(fontSize: 24)),
-                ],
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter a password';
-                  }
-                  return null;
-                },
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
-                child: RaisedButton(
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {
-                      Scaffold.of(context).showSnackBar(
-                          SnackBar(content: Text('Processing Data')));
-                    } else {
-                      Scaffold.of(context).showSnackBar(SnackBar(
-                          content: Text('Username or Password incorrect')));
-                    }
-                  },
-                  child: Text('Login'),
+        Container(
+          padding: EdgeInsets.only(top: 100),
+          child: Text(
+            'Caos',
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(vertical: 0, horizontal: 50),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Username', style: TextStyle(fontSize: 24)),
+                  ],
                 ),
-              )
-            ],
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a username';
+                    }
+                    return null;
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Password', style: TextStyle(fontSize: 24)),
+                  ],
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a password';
+                    }
+                    return null;
+                  },
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 60, top: 20),
+                  child: ButtonTheme(
+                    minWidth: 200.0,
+                    height: 50.0,
+                    buttonColor: Colors.transparent,
+                    child: RaisedButton(
+                      child: Text('Login'),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(18.0),
+                          side: BorderSide(color: Colors.red)),
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          Scaffold.of(context).showSnackBar(
+                              SnackBar(content: Text('Processing Data')));
+                        } else {
+                          Scaffold.of(context).showSnackBar(SnackBar(
+                              content: Text('Username or Password incorrect')));
+                        }
+                      },
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         )
       ],
