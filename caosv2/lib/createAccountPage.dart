fa26dbebc60b391efdc0695e19ba5cb7ebba1960
L0gan012 <logan.sauer.012@gmail.com>
2019-11-02 18:53:55 -0400

    createAccountPage

--- caosv2/lib/createAccountPage.dart
+++ caosv2/lib/createAccountPage.dart
@@ -15,20 +15,11 @@ class CreateAccountPageState extends State<CreateAccountPage> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => HomePage(new User())),
-          );
-        },
-        child: Icon(Icons.near_me),
-      ),
       backgroundColor: Colors.grey[300],
       appBar: AppBar(
         backgroundColor: Colors.grey[300],
         elevation: 0,
+        centerTitle: true,
         leading: IconButton(
           icon: Icon(
             Icons.arrow_back_ios,
@@ -42,6 +33,131 @@ class CreateAccountPageState extends State<CreateAccountPage> {
           'create an account',
           style: TextStyle(color: Colors.black54, fontFamily: 'Puritan'),
         ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+            width: width * .666,
+            height: height * .05,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              border: Border.all(
+                width: 3,
+                color: Colors.transparent,
+              ),
+            ),
+            child: TextField(
+              cursorColor: Colors.blue[100],
+              style: TextStyle(
+                fontFamily: 'Puritan',
+                fontSize: 25,
+              ),
+              decoration: InputDecoration.collapsed(
+                  hintText: 'First Name', hintStyle: TextStyle(fontSize: 20)),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+            width: width * .666,
+            height: height * .05,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              border: Border.all(
+                width: 3,
+                color: Colors.transparent,
+              ),
+            ),
+            child: TextField(
+              cursorColor: Colors.blue[100],
+              style: TextStyle(
+                fontFamily: 'Puritan',
+                fontSize: 25,
+              ),
+              decoration: InputDecoration.collapsed(
+                  hintText: 'Last Name', hintStyle: TextStyle(fontSize: 20)),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+            width: width * .666,
+            height: height * .05,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              border: Border.all(
+                width: 3,
+                color: Colors.transparent,
+              ),
+            ),
+            child: TextField(
+              cursorColor: Colors.blue[100],
+              style: TextStyle(
+                fontFamily: 'Puritan',
+                fontSize: 25,
+              ),
+              decoration: InputDecoration.collapsed(
+                  hintText: 'Age', hintStyle: TextStyle(fontSize: 20)),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+            width: width * .666,
+            height: height * .05,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              border: Border.all(
+                width: 3,
+                color: Colors.transparent,
+              ),
+            ),
+            child: TextField(
+              cursorColor: Colors.blue[100],
+              style: TextStyle(
+                fontFamily: 'Puritan',
+                fontSize: 25,
+              ),
+              decoration: InputDecoration.collapsed(
+                  hintText: 'Email', hintStyle: TextStyle(fontSize: 20)),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+            width: width * .666,
+            height: height * .05,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+              border: Border.all(
+                width: 3,
+                color: Colors.transparent,
+              ),
+            ),
+            child: TextField(
+              cursorColor: Colors.blue[100],
+              style: TextStyle(
+                fontFamily: 'Puritan',
+                fontSize: 25,
+              ),
+              decoration: InputDecoration.collapsed(
+                  hintText: 'Password', hintStyle: TextStyle(fontSize: 20)),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage(new User())),
+          );
+        },
+        child: Icon(Icons.near_me),
       ),
     );
   }
